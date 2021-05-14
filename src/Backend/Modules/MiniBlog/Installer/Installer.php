8e26885a5f56c8964ec0d65221aa2741558b0a68
Bernardusbron <30869776+Bernardusbron@users.noreply.github.com>
2021-05-14 18:28:04 +0200

    ModuleExtraType function included
    Changed installer to include the ModuleExtraType function.

--- src/Backend/Modules/MiniBlog/Installer/Installer.php
+++ src/Backend/Modules/MiniBlog/Installer/Installer.php
@@ -2,6 +2,7 @@
 namespace Backend\Modules\MiniBlog\Installer;
 
 use Backend\Core\Installer\ModuleInstaller;
+use Common\ModuleExtraType;
 
 /**
  * This action will install the mini blog module.
@@ -12,26 +13,21 @@ class Installer extends ModuleInstaller
 {
     /**
      * Install the module.
+     * 'block' and 'widget' replaced by ModuleExtraType::block() and widget() 
      */
     public function install()
     {
         $this->importSQL(dirname(__FILE__).'/Data/install.sql');
-
         $this->addModule('MiniBlog');
-
         $this->importLocale(dirname(__FILE__).'/Data/locale.xml');
-
         $this->makeSearchable($this->getModule());
         $this->setModuleRights(1, $this->getModule());
-
         $this->setActionRights(1, $this->getModule(), 'Index');
         $this->setActionRights(1, $this->getModule(), 'Add');
         $this->setActionRights(1, $this->getModule(), 'Edit');
         $this->setActionRights(1, $this->getModule(), 'Delete');
-
-        $this->insertExtra($this->getModule(), 'block', 'MiniBlog');
-        $this->insertExtra($this->getModule(), 'widget', 'RecentPosts', 'recentposts');
-
+        $this->insertExtra($this->getModule(), ModuleExtraType::block(), 'MiniBlog');
+        $this->insertExtra($this->getModule(), ModuleExtraType::widget(), 'RecentPosts', 'recentposts');
         $navigationModulesId = $this->setNavigation(null, 'Modules');
         $this->setNavigation($navigationModulesId, $this->getModule(), 'MiniBlog/Index', array('MiniBlog/Add', 'MiniBlog/Edit'));
     }
